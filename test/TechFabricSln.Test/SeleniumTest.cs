15e8582c3aa06fef1622c90c2484ca1be8b53908
Ihor Seleznov <ihor@techfabric.co>
2019-11-11 18:35:28 +0200

    Add Selenium Tests

---
+++ test/TechFabricSln.Test/SeleniumTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TechFabricSln.Test
+{
+    class SeleniumTest
+    {
+        [Test]
+        [Category("UITests")]
+        public void Configure_PaymentMethods()
+        {
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(40));
+            driver.Navigate().GoToUrl("https://www.microsoft.com/");
+
+            Thread.Sleep(10000);
+
+            string Windows_text = driver.FindElement(By.Id("shellmenu_1")).Text;
+            Assert.AreEqual("Windows", Windows_text);
+        }
+    }
+}
