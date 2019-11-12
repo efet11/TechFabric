9fab7db9c76f0c3a0641093647f26abd94aa955f
Ihor Seleznov <ihor@techfabric.co>
2019-11-12 10:44:55 +0200

    change driver

--- test/TechFabricSln.Test/SeleniumTest.cs
+++ test/TechFabricSln.Test/SeleniumTest.cs
@@ -4,6 +4,8 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 
@@ -15,7 +17,7 @@ namespace TechFabricSln.Test
         [Category("UITests")]
         public void VisitMicrosoft_CheckWindowsMenu()
         {
-            IWebDriver driver = new ChromeDriver(".");
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             driver.Navigate().GoToUrl("https://www.microsoft.com/");
 
             Thread.Sleep(10000);
